3fcb37f3f033499ecfab074038ee5478b1952f92
JavaTota <jomigone@gmail.com>
2026-04-10 15:55:39 -0500

    Add select query to retrieve and display orders with user and product details

--- assignment.py
+++ assignment.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, Integer, String, Table, create_engine
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, Integer, String, Table, create_engine, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column, relationship
 from typing import List
 
@@ -26,6 +26,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable = False)
     email: Mapped[str] = mapped_column(String(100), unique=True)
+
     orders: Mapped[List['Order']] = relationship('Order', secondary=user_orders, back_populates='users')
 
 class Product(Base):
@@ -33,6 +34,7 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     price: Mapped[int] = mapped_column(Integer, nullable=False)
+
     orders: Mapped[List['Order']] = relationship('Order', secondary=product_orders, back_populates='products')
 
 class Order(Base):
@@ -40,6 +42,7 @@ class Order(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     quantity: Mapped[int] = mapped_column(Integer, nullable=False)
     status: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
     users: Mapped[List['User']] = relationship('User', secondary=user_orders, back_populates='orders')
     products: Mapped[List['Product']] = relationship('Product', secondary=product_orders, back_populates='orders')
 
@@ -47,3 +50,57 @@ class Order(Base):
 Base.metadata.create_all(engine)
 
 session = Session(engine)
+
+# user1 = User(name="Alice", email="alice@gmail.com")
+# user2 = User(name="Marco", email="marco@gmail.com")
+
+# product1 = Product(name="Laptop", price=1200)
+# product2 = Product(name="Phone", price=800)
+# product3 = Product(name="Headphones", price=150)
+
+# order1 = Order(quantity=1, status=True)
+# order2 = Order(quantity=2, status=False)
+# order3 = Order(quantity=3, status=True)
+# order4 = Order(quantity=4, status=False)
+
+# order1.users.append(user1)
+# order2.users.append(user1)
+# order3.users.append(user2)
+# order4.users.append(user2)
+
+# order1.products.append(product1)
+# order2.products.extend([product2, product3])
+# order3.products.append(product1)
+# order4.products.extend([product1, product2, product3])
+
+# session.add_all([
+#     user1, user2,
+#     product1, product2, product3,
+#     order1, order2, order3, order4
+# ])
+# session.commit()
+
+# Retrieve all users and print their information.
+
+# query = session.query(User)
+# users = session.execute(query).scalars().all()
+
+# for user in users:
+#     print(f"User: {user.name}, Email: {user.email}")
+
+# Retrieve all products and print their name and price.
+# query = session.query(Product)
+# products = session.execute(query).scalars().all()
+
+# for product in products:
+#     print(f"Product: {product.name}, Price: {product.price}")
+
+# Retrieve all orders, showing the user’s name, product name, and quantity.
+orders = session.scalars(select(Order)).all()
+print("Orders found:", len(orders))
+
+for order in orders:
+    for user in order.users:
+            for product in order.products:
+                print(f"User: {user.name}, Product: {product.name}, Quantity: {order.quantity}")
+
